1f20400cec7f40101129ef0a215888f3c145ff95
Nathan W <nate01776@gmail.com>
2017-01-27 12:43:45 -0500

    added FB link to page

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 #SUMMER BEARD BAND SITE
 This site was built for a Boston based rock band. The single page app is built in React.
 
-All background/header images are sourced from [Ernst Haeckel's Kunstformen def Natur (1904)](https://commons.wikimedia.org/wiki/Kunstformen_der_Natur) 
+All background/header images are sourced from [Ernst Haeckel's Kunstformen def Natur (1904)](https://commons.wikimedia.org/wiki/Kunstformen_der_Natur)
+
+Band Facebook: https://www.facebook.com/summerbeardmusic
--- src/Social.js
+++ src/Social.js
@@ -5,7 +5,9 @@ class Social extends Component {
   render() {
     return (
       <div className="shows-header">
-        <i className="fa fa-facebook-square fa-3x social-logo facebook" aria-hidden="true"></i>
+        <a href="https://www.facebook.com/summerbeardmusic">
+          <i className="fa fa-facebook-square fa-3x social-logo facebook" aria-hidden="true"></i>
+        </a>
         <i className="fa fa-instagram fa-3x social-logo instagram" aria-hidden="true"></i>
         <i className="fa fa-twitter-square fa-3x social-logo twitter" aria-hidden="true"></i>
       </div>
