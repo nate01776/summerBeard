2552da7ea6074e4078b274505867cd03324a1e7b
Nathan W <nate01776@gmail.com>
2017-02-10 15:47:57 -0500

    scaled social icons on smaller screens, scaled news/tour dates div larger

--- src/Social.js
+++ src/Social.js
@@ -3,19 +3,24 @@ import './App.css';
 
 class Social extends Component {
   render() {
+    var iconSize = "fa-2x"
+    if (document.documentElement.clientWidth > 601) {
+      iconSize = "fa-3x"
+    }
+
     return (
       <div className="shows-header">
         <a href="https://www.facebook.com/summerbeardmusic">
-          <i className="fa fa-facebook-square fa-3x social-logo facebook" aria-hidden="true"></i>
+          <i className={"fa fa-facebook-square " + iconSize + " social-logo facebook"} aria-hidden="true"></i>
         </a>
         <a href="https://www.instagram.com/summerbeard/">
-          <i className="fa fa-instagram fa-3x social-logo instagram" aria-hidden="true"></i>
+          <i className={"fa fa-instagram  " + iconSize + " social-logo instagram"} aria-hidden="true"></i>
         </a>
         <a href="https://twitter.com/search?q=%23summerbeard&src=typd&lang=en">
-          <i className="fa fa-twitter-square fa-3x social-logo twitter" aria-hidden="true"></i>
+          <i className={"fa fa-twitter-square " + iconSize + " social-logo twitter"} aria-hidden="true"></i>
         </a>
         <a href="https://summerbeard.bandcamp.com/releases">
-          <i className="fa fa-bandcamp fa-3x social-logo bandcamp" aria-hidden="true"></i>
+          <i className={"fa fa-bandcamp " + iconSize + " social-logo bandcamp"} aria-hidden="true"></i>
         </a>
       </div>
 
