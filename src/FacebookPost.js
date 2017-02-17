97709f7ed912cb27a427d501d9ef01db0159ef60
Nathan W <nate01776@gmail.com>
2017-02-17 13:53:32 -0500

    added Facebook post images

--- src/FacebookPost.js
+++ src/FacebookPost.js
@@ -8,10 +8,7 @@ const FacebookPost = props => {
     let date = "";
     let time = "";
     let link = "http://www.facebook.com/" + importData.id
-
-    let pullPostData = function(request) {
-
-    }
+    let image = "http://graph.facebook.com/" + props.postData.id.split("_")[1] + "/picture"
 
     let formatTime = function(input) {
       let dates = {1: "January", 2: "Febuary", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
@@ -29,14 +26,21 @@ const FacebookPost = props => {
     };
 
     formatTime(postDate)
-    pullPostData(importData)
 
     return (
 
-        <div className="facebook-post">
-          <p>{message}</p>
-          <a href={link}><h2><i className={"fa fa-facebook-square facebook-logo"} aria-hidden="true"></i>{time} - {date}<i className="fa fa-caret-right arrow" aria-hidden="true"></i></h2></a>
+        <div>
+          <div className="facebook-post">
+            <img src={image} className="post-image"/>
+            <p>
+              {message}
+              <a href={link}><h2><i className={"fa fa-facebook-square facebook-logo"} aria-hidden="true"></i>{time} - {date}<i className="fa fa-caret-right arrow" aria-hidden="true"></i></h2></a>
+            </p>
+          </div>
+          <div>
+          </div>
         </div>
+
     );
 }
 
--- src/News.js
+++ src/News.js
@@ -7,7 +7,7 @@ const News = props => {
 
   let handleNewsData = function (data) {
     for (var i=0; i < data.length; i++) {
-      if (data[i].message != undefined) {
+      if (data[i].message != undefined && i < 20) {
         recentPosts.push(<FacebookPost key={i} postData={data[i]}/>)
       }
     }
