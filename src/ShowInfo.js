8b21e692f660354ca977e4f8893bf3c5c46fda8e
Nathan W <nate01776@gmail.com>
2017-05-09 19:12:47 -0400

    resolved edge case handling

--- src/ShowInfo.js
+++ src/ShowInfo.js
@@ -5,16 +5,22 @@ const ShowInfo = props => {
     let importData = props.showData;
     let importPlace = props.showPlace
     let dateInfo = new Date(importData.start_time);
+    let name = importData.name
     let date = "";
     let time = "";
-    let description = importData.description.substring(0, 200) + " ...";
+    let description = "";
+    let city = "";
+    if (importData.description !== undefined) {
+      description = importData.description.substring(0, 200) + " ...";
+    } else if (importPlace["location"] !== undefined) {
+      city = importPlace["location"]["city"] + ", " + importPlace["location"]["state"]
+    }
     let location = importPlace["name"];
-    let city = importPlace["location"]["city"] + ", " + importPlace["location"]["state"]
     let link = "https://www.facebook.com/events/" + importData.id;
 
     let formatTime = function() {
       let dates = {1: "January", 2: "Febuary", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
-      date = dates[dateInfo.getMonth()] + " " + dateInfo.getDate() + "th"
+      date = dates[dateInfo.getMonth() + 1] + " " + dateInfo.getDate() + "th"
       let hour = dateInfo.getHours()
       let minute = dateInfo.getMinutes()
       if (minute === 0) {
@@ -30,7 +36,7 @@ const ShowInfo = props => {
 
     return (
         <tr className="data-row">
-          <td className="show-date-child"><a href={link}>{time}<br />{date}</a></td><td><a href={link}>{location}<br /><i>{city}</i></a></td><td><a href={link}>{description}</a></td>
+          <td className="show-date-child"><a href={link}>{time}<br />{date}</a></td><td><a href={link}>{location}<br /><i>{city}</i></a></td><td><a href={link}>{name}<br />{description}</a></td>
         </tr>
     );
 }
