0ffa91140de4183a361a2e75ed1a323394fc7e0b
Nathan Wright <nate01776@gmail.com>
2017-12-28 19:44:36 -0500

    update netlify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/index.css';
-
+// test
 ReactDOM.render(
   <App />,
   document.getElementById('root')
